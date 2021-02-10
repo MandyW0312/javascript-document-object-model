6de05dd4886159b92dadbab4f587a75734da4cad
Mandy Wade <lightninggirl413@hotmail.com>
2021-02-10 10:59:46 -0500

    basic click handling of all the squares

--- public/main.js
+++ public/main.js
@@ -1,7 +1,15 @@
 function main() {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+  const allSquares = document.querySelectorAll('li')
+
+  function handleClickSquare(event) {
+    const thingClickedOn = event.target
+
+    thingClickedOn.textContent = 'X'
   }
+
+  allSquares.forEach(square =>
+    square.addEventListener('click', handleClickSquare)
+  )
 }
 
 document.addEventListener('DOMContentLoaded', main)
